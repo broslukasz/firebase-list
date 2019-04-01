dbab78de285bf6ad371ff5eb930971cb2ed0aaa3
lubros <lukasz.bros.sp@gmail.com>
2019-04-01 19:36:12 +0200

    app prepared for firebase deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'IzaSyC1F-MnDyVM3aoe2RsgIzwtLGKRsl1aWkU',
+    authDomain: 'ubros-firebase-list.firebaseapp.com',
+    databaseURL: 'ttps://lubros-firebase-list.firebaseio.com',
+    projectId: 'ubros-firebase-list',
+    storageBucket: 'ubros-firebase-list.appspot.com',
+    messagingSenderId: '098128437865'
+  }
 };
