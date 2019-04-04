804120f230dab0a7ead4d62ee1d742527ed9e84f
lubros <lukasz.bros.sp@gmail.com>
2019-04-04 13:59:36 +0200

    test fixed

--- src/app/users/users.component.spec.ts
+++ src/app/users/users.component.spec.ts
@@ -2,19 +2,15 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { UsersComponent } from './users.component';
 import { UserService } from './user.service';
-import { instance, mock, when } from 'ts-mockito';
+import { instance, mock } from 'ts-mockito';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { MatDialog, MatTableModule } from '@angular/material';
-import { usersFactory } from '../../test/users-factory';
-import { of } from 'rxjs/internal/observable/of';
 
 describe('UsersComponent', () => {
   let component: UsersComponent;
   let fixture: ComponentFixture<UsersComponent>;
 
   const userServiceMock: UserService = mock(UserService);
-
-  when(userServiceMock.findAll()).thenReturn(of([usersFactory.build()]));
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
